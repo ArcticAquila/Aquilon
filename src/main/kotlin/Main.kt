c9c09800173ca5d34bf650714c11a6141332b8f2
ArcticAquila <franklinazriel6942@outlook.com>
2023-06-29 12:48:20 +0000

    fix output logging

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,33 @@ import io.github.cdimascio.dotenv.Dotenv
 import io.github.cdimascio.dotenv.dotenv
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
-import net.dv8tion.jda.api.hooks.EventListener
-import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.utils.cache.CacheFlag
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.util.*
 
 fun main() {
+
+    // Check if .env file exists
+    val envFile = File(".env")
+    if (!envFile.exists()) {
+        try {
+            // Generate .env file
+            FileWriter(envFile).use {
+                it.write("TOKEN=\n")
+                //it.write("SHARD_COUNT=\n")
+                it.write("ACTIVITY_TYPE=\n")
+                it.write("ACTIVITY_DETAIL=\n")
+            }
+            println(".env file generated successfully.")
+        } catch (e: IOException) {
+            println("Failed to generate .env file: ${e.message}")
+        }
+    }
+
     val dotenv: Dotenv = dotenv()
+
     val token: String = dotenv["TOKEN"]
     val builder = JDABuilder.createDefault(token)
 
@@ -16,11 +37,24 @@ fun main() {
     // Enable the bulk delete event
     builder.setBulkDeleteSplittingEnabled(false)
     // Set activity (like "playing Something")
-    builder.setActivity(Activity.watching("Your Mom"))
+    val activityType: String? = dotenv["ACTIVITY_TYPE"]
+    val activityDetail: String = dotenv["ACTIVITY_DETAIL"]
+    val activity: Activity = when (activityType?.lowercase(Locale.getDefault())) {
+        "playing" -> Activity.playing(activityDetail)
+        "watching" -> Activity.watching(activityDetail)
+        "listening" -> Activity.listening(activityDetail)
+        //"streaming" -> Activity.streaming(activityDetail, "https://twitch.tv/yourstream")
+        else -> Activity.playing(activityDetail)
+    }
+    builder.setActivity(activity)
 
     // Register the listeners
     builder.addEventListeners(ReadyListener())
     builder.addEventListeners(MessageListener())
+
+    // Register slash commands
+    //val slashCommandHandler = SlashCommandHandler(builder)
+    //slashCommandHandler.registerCommands()
 
     builder.build()
 }
